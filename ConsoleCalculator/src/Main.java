8ee9e8919cfdab8f94a4d6c849093240e1c99a71
zubarevam96 <52414134+zubarevam96@users.noreply.github.com>
2019-07-01 20:14:48 +0300

    Delete Main.java

--- ConsoleCalculator/src/Main.java
+++
@@ -1,9 +0,0 @@
-import UserInterfaces.ConsoleInterface;
-
-public class Main {
-
-    public static void main(String[] args) {
-        // запуск консольного интерфейса
-        ConsoleInterface.Start();
-    }
-}
